5a9953fa0a50c9569b0b03432f06923547029992
Kevin <kevinfonseca807@gmail.com>
2025-03-07 10:57:45 -0500

    if simple

---
+++ if_simple.py
@@ -0,0 +1,11 @@
+'''
+sintaxis  if simple : 
+if condicional : 
+ instruccion1 
+ instruccion2 
+'''
+a = 9
+b = 3
+if a > b :
+    print("a es mayor que b") 
+    print ("y hoy es viernes y el cuerpo lo sabe")
