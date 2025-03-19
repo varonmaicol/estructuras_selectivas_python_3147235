811fb4b107ccabc0bce365286afda88bb1ed195a
David <davidsmoraleslopez10@gmail.com>
2025-03-19 07:47:31 -0500

    if else

---
+++ if_else.py
@@ -0,0 +1,28 @@
+'''
+if_else 
+determinar lo tipos de caminos 
+de ejecucion basados en la  
+evaluacion de una condicional 
+sintaxis 
+if condicional : 
+ instruccion1 
+ instruccion2 
+else :
+ instruccion3
+ instruccion4  
+'''
+#ejemplo 
+#elabore un programa de python 
+# que determine si  una persona es mayor o menor de edad 
+# y por tanto, habilitada para 
+#votar 
+edad = 20
+documento = False
+if edad >= 18 and documento==True:
+    print ("usted es mayor de edad ")
+    print("puede votar gansgter")
+else :
+    print("usted es menor de edad")
+    print("O")
+    print("no puede votar bro")
+print ("fin del programa")
